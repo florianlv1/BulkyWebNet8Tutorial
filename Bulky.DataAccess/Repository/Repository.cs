05544a3c5af265a797d4e9074dc6b10e20e9fce8
Florian Nguyen Van <florian.nguyen.van@aua24ag.de>
2023-12-13 16:06:56 +0100

    IRepository

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository
+   
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+
+        internal DbSet<T> DbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.DbSet = db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = DbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = DbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            DbSet.RemoveRange(entities);
+        }
+    }
+}
